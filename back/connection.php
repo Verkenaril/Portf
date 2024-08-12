63a101c7bea6c1879cdeb28ed35ae8ff71aa21b0
verk <myEmail@example.com>
2024-08-12 06:33:46 +0300

    change

--- back/connection.php
+++ back/connection.php
@@ -7,7 +7,7 @@ $options =
 ];
 try
 {
-    $connection = new PDO("mysql:host=http://83.222.11.85:7771;dbname=audiojs;charset=utf8", "nigga", "mypassnigga", $options);
+    $connection = new PDO("mysql:host='http://83.222.11.85:7771';dbname=audiojs;charset=utf8", "nigga", "mypassnigga", $options);
 }
 catch(PDOException $e)
 {
