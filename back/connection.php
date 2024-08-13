f2bd79ae6976a3cf8f793f53e067779d04526ae9
verk <myEmail@example.com>
2024-08-13 20:46:10 +0300

    change

--- back/connection.php
+++ back/connection.php
@@ -1,13 +1,14 @@
 <?php
 
 $connection;
+$servername = "83.222.11.85";
 $options = 
 [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
 ];
 try
 {
-    $connection = new PDO("mysql:host='http://83.222.11.85:7771';dbname=audiojs;charset=utf8", "nigga", "mypassnigga", $options);
+    $connection = new PDO("mysql:host=$servername;port=7771;dbname=audiojs;charset=utf8", "nigga", "mypassnigga", $options);
 }
 catch(PDOException $e)
 {
